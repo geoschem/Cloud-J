9e8a709110ff3c1f6af0aee1ad6030f7c31e4b65
Lizzie Lundgren <elundgren@seas.harvard.edu>
2021-10-04 13:39:24 -0400

    Remove old commented out code for alternate slash
    Signed-off-by: Lizzie Lundgren <elundgren@seas.harvard.edu>

--- src/Core/fjx_init_mod.f90
+++ src/Core/fjx_init_mod.f90
@@ -33,8 +33,6 @@
 
       NUN = JXUNIT
       open (NUN,FILE='tables/CJ77_inp.dat',status='old',form='formatted')
-!ewl!      open (NUN,FILE='tables\CJ77_inp.dat',status='old',form='formatted')
-!SJ!      open (NUN,FILE='tables/CJ77_inp.dat',status='old',form='formatted')
       read (NUN,'(a120)',err=4) TIT_SPEC
          write(6,'(a)') TIT_SPEC
       read (NUN,'(e10.3)',err=4) RAD
