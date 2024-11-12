6cc67605d5f82a0bd730be803b4f097477c57ab8
Kyle Shores <kyle.shores44@gmail.com>
2024-11-12 09:19:42 -0600

    reverting removal of netcdf

--- CMakeScripts/try_compile.F90
+++ CMakeScripts/try_compile.F90
@@ -1,4 +1,6 @@
 program try_compile
+    use netcdf
+
     implicit none
 
     character (len = *), parameter :: FILE_NAME = "simple_xy.nc"
@@ -19,6 +21,37 @@ program try_compile
     !$OMP END PARALLEL
 #endif
 
+    ! Try NetCDF-F
+    do x = 1, NX
+       do y = 1, NY
+          data_out(y, x) = (x - 1) * NY + (y - 1)
+       end do
+    end do
+    write(*,*) 'Creating NetCDF file'
+    call check( nf90_create(FILE_NAME, NF90_CLOBBER, ncid) )
+    call check( nf90_def_dim(ncid, "x", NX, x_dimid) )
+    call check( nf90_def_dim(ncid, "y", NY, y_dimid) )
+
+    ! The dimids array is used to pass the IDs of the dimensions of
+    ! the variables. Note that in fortran arrays are stored in
+    ! column-major format.
+    dimids =  (/ y_dimid, x_dimid /)
+
+    call check( nf90_def_var(ncid, "data", NF90_INT, dimids, varid) )
+
+    call check( nf90_enddef(ncid) )
+    call check( nf90_put_var(ncid, varid, data_out) )
+    call check( nf90_close(ncid) )
+    write(*,*) 'Finished creating the NetCDF file'
+
   contains
+    subroutine check(status)
+      integer, intent ( in) :: status
+
+      if(status /= nf90_noerr) then
+        print *, trim(nf90_strerror(status))
+        stop "Stopped"
+      end if
+    end subroutine check
 
 end program
